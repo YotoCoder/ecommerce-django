9fa085bd0cf31d7ce461a2d8e996a8ea93563d12
Yonathan Soto <yotoelectronics@gmail.com>
2022-08-22 04:13:42 -0500

    'semi'

---
+++ static/js/cart.js
@@ -0,0 +1,52 @@
+
+
+function toCart(obj){
+
+	obj.qty = document.getElementById('qty').value
+
+    console.log(obj.qty)
+
+    data = localStorage.setItem('items', JSON.stringify(obj))
+
+    console.log(JSON.parse(localStorage.getItem('items')))
+
+};
+
+
+
+function renderCart(){
+    
+    var cartList = document.getElementById('cart-list')
+
+    let items = JSON.parse(sessionStorage.getItem('items'))
+
+    let item = items
+
+    console.log(item)
+
+    cartList.innerHTML = 
+    
+    `
+            <div class="product-widget">
+
+            <div class="product-img">
+                <img src=${item.img} alt="">
+            </div>
+            
+            <div class="product-body">
+                <h3 class="product-name"><a href="/product-detail/${item.id}">${item.name}</a></h3>
+                <h4 class="product-price"><span class="qty">${item.qty}x</span>$ ${item.price}</h4>
+            </div>
+            
+            <button class="delete" value="${item.id}"><i class="fa fa-close"></i></button>
+            
+            </div>
+    
+    `;
+    
+    
+    
+};
+
+
+document.addEventListener("load", renderCart());
