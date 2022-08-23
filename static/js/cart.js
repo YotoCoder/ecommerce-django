a58738bd496b62429592820aa168bffd30d4bda5
Yonathan Soto <yotoelectronics@gmail.com>
2022-08-23 10:49:35 -0500

    ok

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,81 +1,88 @@
+class Local{
+	constructor(key){
+		this.key = key
+		this.item = this.get()
 
-function toCart(obj){
+		if(this.item === null){
+			this.set([])
+		}
+	}
 
-    let carrito = []
+	set(value){
+		localStorage.setItem(this.key, JSON.stringify(value))
+	}
 
-    var storage = JSON.parse(localStorage.getItem('items'))
+	get(){
+		return JSON.parse(localStorage.getItem(this.key))
+	}
+
+	add(value){
+		let data = this.get()
+		data.push(value)
+		this.set(data)
+	}
+
+	delete(id){
+		let list = this.get()
+	    for(var i = list.length - 1; i>=0 ;i--){
+	        if(list[i].id == id)
+	            list.splice(i, 1);
+	    }
+	   	this.set(list)
+	}
+}
+let cart = new Local('items')
 
 
-    if(carrito){
-        console.log('existe carrito')
-
-        carrito.push({uno:'1'})
-
-        console.log(`carrito:${carrito}`)
-
-
-
-    }else{
-        console.log('No existe carrito')
-
-    }
-
-    
-
-
+function addToCar(obj){
    
+    cart.add(obj)
+    console.log(cart.get())
+	renderCart()
+}
 
-    // console.log(obj.qty);
-
-    // var itemsLocal = JSON.parse(localStorage.getItem('items'))
-    
-    
-
-
-    if(obj.qty != 1){
-        obj.qty = document.getElementById('qty').value;
-    }
-
-   
-
-    data = localStorage.setItem('items', JSON.stringify(obj));
-
-    //console.log(JSON.parse(localStorage.getItem('items')));
-
-    renderCart()
-};
+function createItem(item){
+	const container = document.createElement('div')
+	container.classList.add('product-widget')
+	container.innerHTML = 
+	`
+						<div class="product-img">
+                            <img src=${item.img} alt="">
+                        </div>
+                        
+                        <div class="product-body">
+                            <h3 class="product-name"><a href="/product-detail/${item.id}">${item.name}</a></h3>
+                            <h4 class="product-price"><span class="qty">${item.qty}x</span>$ ${item.price}</h4>
+                        </div>
+                        
+                        <button class="delete" value="${item.id}"><i class="fa fa-close"></i></button>
 
 
+	`;
+
+	container.querySelector('button').addEventListener('click', function(){
+		cart.delete(item.id)
+		container.remove()
+	});
+
+	return container
+}
 
 function renderCart(){
 
-
     var cartList = document.getElementById('cart-list');
 
-    let items = JSON.parse(localStorage.getItem('items'));
-
-    let item = items;
+    let items = cart.get()
 
     //console.log(item);
 
-    cartList.innerHTML = 
-    `
-            <div class="product-widget">
+	cartList.remove()
 
-            <div class="product-img">
-                <img src=${item.img} alt="">
-            </div>
-            
-            <div class="product-body">
-                <h3 class="product-name"><a href="/product-detail/${item.id}">${item.name}</a></h3>
-                <h4 class="product-price"><span class="qty">${item.qty}x</span>$ ${item.price}</h4>
-            </div>
-            
-            <button class="delete" value="${item.id}"><i class="fa fa-close"></i></button>
-            
-            </div>
-    
-    `;
+    items.forEach(item => {
+		product = createItem(item)
+		cartList.appendChild(product)
+    });
+
 };
 
 
